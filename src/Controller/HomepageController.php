675b011d3a3d358b428be00d938db59468069002
Filip Bozanovic <bozanovic.filip@q-software.com>
2018-01-13 12:05:28 +0100

    Add login/logout buttons to base.html.twig, inherit layout from base for homepage:index

--- src/Controller/HomepageController.php
+++ src/Controller/HomepageController.php
@@ -2,23 +2,15 @@
 
 namespace App\Controller;
 
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
-use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\HttpFoundation\Response;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
 class HomepageController
 {
-
+    /**
+     * @Template
+     */
     public function indexAction()
     {
-        return new JsonResponse(['id'=>1,'name'=>'John']);
-    }
-
-    /**
-     * @Route("/hello")
-     */
-    public function helloAction()
-    {
-        return new Response('Hello');
+        // @todo
     }
 }
