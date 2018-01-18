b270291b6dc2a8fe2312ba66136afb079e6bb894
Filip Bozanovic <bozanovic.filip@q-software.com>
2018-01-18 13:57:16 +0100

    Add Point, repository and migration

---
+++ src/Repository/PointRepository.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Point;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Symfony\Bridge\Doctrine\RegistryInterface;
+
+class PointRepository extends ServiceEntityRepository
+{
+    public function __construct(RegistryInterface $registry)
+    {
+        parent::__construct($registry, Point::class);
+    }
+
+    /*
+    public function findBySomething($value)
+    {
+        return $this->createQueryBuilder('p')
+            ->where('p.something = :value')->setParameter('value', $value)
+            ->orderBy('p.id', 'ASC')
+            ->setMaxResults(10)
+            ->getQuery()
+            ->getResult()
+        ;
+    }
+    */
+}
