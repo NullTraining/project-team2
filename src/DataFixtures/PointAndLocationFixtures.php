76d6abdf457d50b9055ad4a1e0562e653681172f
Filip Bozanovic <bozanovic.filip@q-software.com>
2018-01-18 14:37:40 +0100

    Update PointFixtures to add Location data

--- src/DataFixtures/PointAndLocationFixtures.php
+++ src/DataFixtures/PointAndLocationFixtures.php
@@ -1,11 +1,12 @@
 <?php
 namespace App\DataFixtures;
 
+use App\Entity\Location;
 use App\Entity\Point;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
 
-class PointFixtures extends Fixture
+class PointAndLocationFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
@@ -25,10 +26,30 @@ class PointFixtures extends Fixture
         $point4->setLatitude(45.819583);
         $point4->setLongitude(16.016235);
 
+        $location1 = new Location();
+        $location1->setStreet('Palmotićeva');
+        $location1->setStreetNumber('20a');
+        $location1->setPostBox('10000');
+        $location1->setCity('Zagreb');
+        $location1->setPoints([$point1]);
+        $point1->setLocation($location1);
+
+        $location2 = new Location();
+        $location2->setStreet('Zagrebački trokut');
+        $location2->setStreetNumber('123');
+        $location2->setPostBox('10000');
+        $location2->setCity('Zagreb');
+        $location2->setPoints([$point2, $point3, $point4]);
+        $point2->setLocation($location2);
+        $point3->setLocation($location2);
+        $point4->setLocation($location2);
+
         $manager->persist($point1);
         $manager->persist($point2);
         $manager->persist($point3);
         $manager->persist($point4);
+        $manager->persist($location1);
+        $manager->persist($location2);
 
         $this->addReference('1-point', $point1);
         $this->addReference('2-point', $point2);
