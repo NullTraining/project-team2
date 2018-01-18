c400ddf7b1fcca9e2fecb4378ef81345009b2049
Filip Bozanovic <bozanovic.filip@q-software.com>
2018-01-18 17:04:46 +0100

    Update HomepageController to fetch Points from the DB

--- src/Controller/HomepageController.php
+++ src/Controller/HomepageController.php
@@ -2,15 +2,32 @@
 
 namespace App\Controller;
 
+use App\Entity\Location;
+use App\Repository\LocationRepository;
+use Doctrine\ORM\PersistentCollection;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
 class HomepageController
 {
     /**
      * @Template
+     * @param LocationRepository $locationRepository
+     *
+     * @return array
      */
-    public function indexAction()
+    public function indexAction(LocationRepository $locationRepository)
     {
-        // @todo
+        $locations = $locationRepository->findAll();
+
+        $pointSets = [];
+
+        /** @var Location $location */
+        foreach ($locations as $location) {
+            /** @var PersistentCollection $points */
+            $points = $location->getPoints();
+            $pointSets[$location->getId()] = $points->toArray();
+        }
+
+        return ['point_sets' => $pointSets];
     }
 }
