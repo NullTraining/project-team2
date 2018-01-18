24ce3c9a9db590a5a63801bb1a75b281778acd2d
Filip Bozanovic <bozanovic.filip@q-software.com>
2018-01-18 14:36:53 +0100

    Add Location entity and repo

---
+++ src/Entity/Location.php
@@ -0,0 +1,140 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\LocationRepository")
+ */
+class Location
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\OneToMany(targetEntity="Point", mappedBy="Location")
+     */
+    private $points;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $street;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $streetNumber;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $postBox;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $city;
+
+    /**
+     * Location constructor.
+     */
+    public function __construct()
+    {
+        $this->points = new ArrayCollection();
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPoints()
+    {
+        return $this->points;
+    }
+
+    /**
+     * @param mixed $points
+     */
+    public function setPoints($points)
+    {
+        $this->points = $points;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getStreet()
+    {
+        return $this->street;
+    }
+
+    /**
+     * @param mixed $street
+     */
+    public function setStreet($street)
+    {
+        $this->street = $street;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getStreetNumber()
+    {
+        return $this->streetNumber;
+    }
+
+    /**
+     * @param mixed $streetNumber
+     */
+    public function setStreetNumber($streetNumber)
+    {
+        $this->streetNumber = $streetNumber;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPostBox()
+    {
+        return $this->postBox;
+    }
+
+    /**
+     * @param mixed $postBox
+     */
+    public function setPostBox($postBox)
+    {
+        $this->postBox = $postBox;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCity()
+    {
+        return $this->city;
+    }
+
+    /**
+     * @param mixed $city
+     */
+    public function setCity($city)
+    {
+        $this->city = $city;
+    }
+}
