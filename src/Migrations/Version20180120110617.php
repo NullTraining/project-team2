aba2dd7f56a8c7e6a1be4aedaff446bbf5a71c03
zgperak <zgperak@gmail.com>
2018-01-20 03:10:23 -0800

    #95 - Updated migration for new relation

--- src/Migrations/Version20180120110617.php
+++ src/Migrations/Version20180120110617.php
@@ -8,7 +8,7 @@ use Doctrine\DBAL\Schema\Schema;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-class Version20180119154853 extends AbstractMigration
+class Version20180120110617 extends AbstractMigration
 {
     public function up(Schema $schema)
     {
@@ -17,7 +17,7 @@ class Version20180119154853 extends AbstractMigration
 
         $this->addSql('ALTER TABLE item ADD location_id INT DEFAULT NULL, DROP location');
         $this->addSql('ALTER TABLE item ADD CONSTRAINT FK_1F1B251E64D218E FOREIGN KEY (location_id) REFERENCES location (id)');
-        $this->addSql('CREATE INDEX IDX_1F1B251E64D218E ON item (location_id)');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_1F1B251E64D218E ON item (location_id)');
     }
 
     public function down(Schema $schema)
@@ -26,7 +26,7 @@ class Version20180119154853 extends AbstractMigration
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
         $this->addSql('ALTER TABLE item DROP FOREIGN KEY FK_1F1B251E64D218E');
-        $this->addSql('DROP INDEX IDX_1F1B251E64D218E ON item');
+        $this->addSql('DROP INDEX UNIQ_1F1B251E64D218E ON item');
         $this->addSql('ALTER TABLE item ADD location LONGTEXT NOT NULL COLLATE utf8_unicode_ci, DROP location_id');
     }
 }
