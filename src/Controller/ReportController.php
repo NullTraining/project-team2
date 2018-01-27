09ad0b71c8f0cf3fa8871865ebfb047a62f64358
tperkovic <tomislav.perkovic@skolskaknjiga.hr>
2018-01-27 18:28:11 +0100

    #104 - added ReportController that will handle submits of lost/found items

---
+++ src/Controller/ReportController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+
+class ReportController
+{
+    /**
+     * @Template
+     *
+     * @return array
+     */
+
+    public function indexAction()
+    {
+
+        return [];
+
+    }
+    /**
+     * @Template
+     *
+     * @return array
+     */
+
+    public function lostAction()
+    {
+
+        return [];
+
+    }
+    /**
+     * @Template
+     *
+     * @return array
+     */
+
+    public function foundAction()
+    {
+
+        return [];
+
+    }
+}
