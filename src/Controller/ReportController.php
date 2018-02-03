9bc18f6541fe3866b5923039a0ebafe6c08ebf95
tperkovic <tomislav.perkovic@skolskaknjiga.hr>
2018-02-03 13:24:09 +0100

    #77 - added function for generating base form

--- src/Controller/ReportController.php
+++ src/Controller/ReportController.php
@@ -2,10 +2,21 @@
 
 namespace App\Controller;
 
+use App\Entity\Item;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use App\Repository\CategoryRepository;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Vich\UploaderBundle\Form\Type\VichImageType;
 
+/**
+ * @Embedded\Embedded
+ */
 class ReportController
 {
+
     /**
      * @Template
      *
@@ -51,5 +62,29 @@ class ReportController
     public function submittedAction()
     {
         return [];
+    }
+
+    /**
+     * @param CategoryRepository $repository
+     * @return mixed
+     */
+    private function getForm(CategoryRepository $repository){
+
+
+        $item = new Item();
+        $form = $this->createFormBuilder($item)
+            ->add('title', TextType::class)
+            ->add('description', TextareaType::class)
+            ->add('category',ChoiceType::class,[
+                'choices'=>$repository->findAll(),
+                'choice_label' => function($category, $key, $index) {
+                    /** @var \App\Entity\Category $category */
+                    return strtoupper($category->getTitle());
+                },
+            ])
+            ->add('pictureFile', VichImageType::class,  array('label' => 'Slika'))
+            ->add('save', SubmitType::class, array('label' => 'Report item'));
+
+        return $form;
     }
 }
